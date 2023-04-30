1371e49f7e901fedf5c3f8132b28e63bd68a6a22
Matheus Ribeiro <matheus.ribeiro.mrs762@gmail.com>
2023-04-29 21:27:15 -0300

    feature: jwt strategy initial implementation

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
+import { JwtStrategy } from './jwt.strategy';
+import { PassportModule } from '@nestjs/passport';
 
 @Module({
-  providers: [AuthService],
-  controllers: [AuthController]
+  imports: [PassportModule.register({ defaultStrategy: 'jwt' })],
+  providers: [AuthService, JwtStrategy],
+  controllers: [AuthController],
+  exports: [PassportModule],
 })
 export class AuthModule {}
