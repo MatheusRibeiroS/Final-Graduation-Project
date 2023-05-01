a7f24c0690a96ba7f8eeccfc0fa93ffc4407865c
Matheus Ribeiro <matheus.ribeiro.mrs762@gmail.com>
2023-05-01 18:29:04 -0300

    feature: authentication (with sample) and enable cors

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
+import { ItemsModule } from './items/items.module';
 
 @Module({
-  imports: [AuthModule],
+  imports: [AuthModule, ItemsModule],
   controllers: [AppController],
   providers: [AppService],
 })
