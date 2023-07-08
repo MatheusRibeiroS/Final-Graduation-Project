370efd66a51ffc77a79cac344bf3454bb4be4298
Matheus Ribeiro <matheus.ribeiro.mrs762@gmail.com>
2023-07-08 15:23:34 -0300

    feature:  login final structure

--- dashboard/src/app/page.tsx
+++ dashboard/src/app/page.tsx
@@ -4,11 +4,11 @@ import SlideOvers from "@/components/Slide-overs";
 import { useSession, signIn, signOut } from "next-auth/react";
 
 export default function Home() {
-  const { data: session } = useSession();
+  const { data: session, status, update } = useSession();
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <button onClick={() => signIn('auth0')}>entrar</button>
+        <button onClick={() => signIn('google')}>entrar</button>
         <button onClick={() => signOut()}>sair</button>
         <Link href="/teste">page</Link>
         <SlideOvers />
